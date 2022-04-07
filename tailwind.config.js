3652d5f6557c32749487405fa520ed04bc926bc7
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-04-07 12:54:16 +0600

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
           "./output/script.js",
         "./src/script.ts"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor:{
+        "very-dark-blue":"hsl(216, 12%, 8%)",
+        "normal-dark-blue":"hsl(216, 12%, 25%)",
+        "Light-Grey":"hsl(217, 12%, 63%)",
+        "Dark-Blue": "hsl(213, 19%, 18%)",
+        "Medium-Grey": "hsl(216, 12%, 54%)"
+      },
+      height:{
+        "vh":"100vh",
+        "fourty":"400px",
+      },
+      width:{
+        "fourty":"40%",
+      },
+      margin:{
+        "20":"20px",
+      }
+    },
   },
   plugins: [],
 }
